3aef4d2ffb479b6dcc956bd183928fc00331b167
lowki93 <budain.kevin@yahoo.Fr>
2015-03-03 15:13:32 +0100

    first commit

---
+++ Data/Data/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Data
+//
+//  Created by kevin Budain on 03/03/2015.
+//  Copyright (c) 2015 kevin Budain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
