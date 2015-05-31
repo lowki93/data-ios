89a3fbf71c19f0ceec19c9e4133897b91720451c
lowki93 <budain.kevin@yahoo.Fr>
2015-06-01 00:31:49 +0200

    fix storyboard

---
+++ Data/Data/TutorialPullUpView.h
@@ -0,0 +1,13 @@
+//
+//  TutorialPullUpView.h
+//  Data
+//
+//  Created by kevin Budain on 31/05/2015.
+//  Copyright (c) 2015 kevin Budain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TutorialPullUpView : UIView
+
+@end
---
+++ Data/Data/TutorialPullUpView.m
@@ -0,0 +1,21 @@
+//
+//  TutorialPullUpView.m
+//  Data
+//
+//  Created by kevin Budain on 31/05/2015.
+//  Copyright (c) 2015 kevin Budain. All rights reserved.
+//
+
+#import "TutorialPullUpView.h"
+
+@implementation TutorialPullUpView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
